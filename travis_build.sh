a3fe59173b50c9204f6695a0902bf60d022f3f60
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2020-08-26 17:17:04 +0200

    Revert "17: build java14 test first"
    This reverts commit ece968c941f212c81599f2d28f3c46b67967c86b.

--- travis_build.sh
+++ travis_build.sh
@@ -4,8 +4,6 @@ set -ev
 
 # install our POMs
 mvn -f releng/io.github.lorenzobettini.xtextutils.releng/pom.xml clean install && \
-# run the Xbase Tycho test project
-mvn -f testlanguages/tychoxbasetestlanguage/io.github.lorenzobettini.tychoxbasetestlanguage.parent/pom.xml clean verify $ADDITIONAL && \
 # run testlanguages using our POMs
 mvn -U -f testlanguages/tychotestlanguage/io.github.lorenzobettini.tychotestlanguage.parent/pom.xml clean verify $ADDITIONAL && \
 # run it again without source code generation (MWE2 and Xtend are skipped)
@@ -13,5 +11,7 @@ mvn -f testlanguages/tychotestlanguage/io.github.lorenzobettini.tychotestlanguag
 # run the pure Maven test project
 mvn -f testlanguages/maventestlanguage/io.github.lorenzobettini.maventestlanguage.parent/pom.xml clean verify && \
 # run it again without source code generation (MWE2 and Xtend are skipped)
-mvn -f testlanguages/maventestlanguage/io.github.lorenzobettini.maventestlanguage.parent/pom.xml clean verify -DskipMWE2 -DskipXtend
+mvn -f testlanguages/maventestlanguage/io.github.lorenzobettini.maventestlanguage.parent/pom.xml clean verify -DskipMWE2 -DskipXtend && \
+# run the Xbase Tycho test project
+mvn -f testlanguages/tychoxbasetestlanguage/io.github.lorenzobettini.tychoxbasetestlanguage.parent/pom.xml clean verify $ADDITIONAL
 
